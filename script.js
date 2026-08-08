26f57935b0c0fd4ea10cdff882af1c0ad11b911d
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-08-08 21:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,6 @@ document.addEventListener("DOMContentLoaded", () => {
             { type: 'h3', text: 'Dear GUNGUN🦋' },
             { type: 'p', text: 'Gungun, main bas yehi dua karta hu ki tum hamesha khush raho. Tumhare chehre ki muskan kabhi kam na ho, kyuki tum sach me har ek khusi deserve karti ho.' },
             { type: 'p', text: 'Hamesha aise hi muskurati rehna, apne sapno ko poora karna aur life me aage badhte rehna.🩺👩‍⚕️🩺' },
-            { type: 'p', text: '' },
             { type: 'p', text: 'Once again, Happy Birthday Gungun!✨🎂', className: 'highlight-bday' },
             { type: 'p', text: 'Take care of yourself.🌸', className: 'signature' },
             { type: 'p', text: '-MANAV', className: 'signature' }
